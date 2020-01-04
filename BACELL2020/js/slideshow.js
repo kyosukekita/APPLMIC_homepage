c7dfa7cf31d7e88902d1223fb4947ac3a07d2ac4
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-01-04 11:25:47 +0900

    Add files via upload

---
+++ BACELL2020/js/slideshow.js
@@ -0,0 +1,21 @@
+<script>
+$(function(){
+  $('.slideshow').each(function(){
+
+    var $slides=$(this).find('img'),
+    slideCount=$slides.length,
+    currentIndex=0;
+
+    $slides.eq(currentIndex).fadeIn();
+
+    setInterval(showNextSlide,8000);
+
+    function showNextSlide (){
+      var nextIndex =(currentIndex+1) % slideCount;
+      $slides.eq(currentIndex).fadeOut();
+      $slides.eq(nextIndex).fadeIn();
+      currentIndex=nextIndex;
+    }
+  });
+});
+</script>
